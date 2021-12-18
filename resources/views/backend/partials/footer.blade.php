c1a486fbd70277256f2c1767484c5d3260cff08c
Mai Anh Nam <canmotcaiten.tn95@gmail.com>
2021-12-19 01:25:42 +0700

    hokhau

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -3,8 +3,8 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-        Anything you want
+        Copyright &copy; Phan Như Phương
     </div>
     <!-- Default to the left -->
-    <strong>BẢN QUYỀN THUỘC VỀ CÔNG AN TỈNH THÁI NGUYÊN <a href="https://adminlte.io"></a></strong>
+    <strong>BẢN QUYỀN THUỘC VỀ CÔNG AN TỈNH THÁI NGUYÊN <a href="{{ url('') }}"></a></strong>
 </footer>
